d0707a72bac124ac6c25a180e253b04050765a3b
Raymond Jolly <raymond.jolly@gmail.com>
2021-09-06 13:33:27 -0400

    initial commit for React Frontend

---
+++ src/components/Admin.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react'
+
+export default class Admin extends Component{
+
+    render(){
+        return (
+            <h1>Manage Categories</h1>
+        )
+    };
+}
