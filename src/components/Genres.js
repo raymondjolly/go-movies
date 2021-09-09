f9cbf0c44e7b642b33e2f33e9aeae9e1476c56da
Raymond Jolly <raymond.jolly@gmail.com>
2021-09-08 23:19:23 -0400

    added the Genre Name to the genres template and cleaned up some of the formatting

--- src/components/Genres.js
+++ src/components/Genres.js
@@ -1,59 +1,64 @@
-import React, {Component, Fragment} from "react";
-import {Link} from "react-router-dom";
+import React, { Component, Fragment } from "react";
+import { Link } from "react-router-dom";
 
 export default class Genres extends Component {
     state = {
         genres: [],
         isLoaded: false,
-        error: null
-    }
+        error: null,
+    };
 
     componentDidMount() {
         fetch("http://localhost:4000/v1/genres")
-            //
             .then((response) => {
-                console.log("Status code is", response.status)
-                if (response.status !== 200) {
+                if (response.status !== "200") {
                     let err = Error;
                     err.message = "Invalid response code: " + response.status;
-                    this.setState({error: err});
+                    this.setState({ error: err });
                 }
                 return response.json();
             })
             .then((json) => {
-                this.setState({
+                this.setState(
+                    {
                         genres: json.genres,
                         isLoaded: true,
                     },
                     (error) => {
                         this.setState({
                             isLoaded: true,
-                            error
+                            error,
                         });
-                    });
+                    }
+                );
             });
     }
 
     render() {
-
-        const {genres, isLoaded, error} = this.state;
-        if(error){
-            return <div>Error: {error.message}</div>
+        const { genres, isLoaded, error } = this.state;
+        if (error) {
+            return <div>Error: {error.message}</div>;
         } else if (!isLoaded) {
             return <p>Loading...</p>;
-        } else
-        return(
-            <Fragment>
-                <h2>Genres</h2>
+        } else {
+            return (
+                <Fragment>
+                    <h2>Genres:</h2>
 
-                <ul>
-                    {genres.map((m) => (
-                        <li key={m.id}>
-                            <Link to={`/genre/${m.id}`}>{m.genre_name}</Link>
-                        </li>
-                    ))}
-                </ul>
-            </Fragment>
-        )
+                    <div className='list-group'>
+                        {genres.map((m) => (
+                                <Link key={m.id} className="list-group-item list-group-item-action"
+                                    to={{
+                                        pathname: `/genre/${m.id}`,
+                                        genreName: m.genre_name,
+                                    }}
+                                >
+                                    {m.genre_name}
+                                </Link>
+                        ))}
+                    </div>
+                </Fragment>
+            );
+        }
     }
 }
--- src/components/OneGenre.js
+++ src/components/OneGenre.js
@@ -7,6 +7,7 @@ export default class OneGenre extends Component{
         movies:[],
         isLoaded:false,
         error:null,
+        genreName:"",
     }
 
     componentDidMount() {
@@ -24,6 +25,7 @@ export default class OneGenre extends Component{
                 this.setState({
                         movies: json.movies,
                         isLoaded: true,
+                        genreName:this.props.location.genreName
                     },
                     (error) => {
                         this.setState({
@@ -35,7 +37,7 @@ export default class OneGenre extends Component{
     }
 
     render() {
-        let { movies, isLoaded, error } = this.state;
+        let { movies, isLoaded, error, genreName } = this.state;
         if(!movies){
             movies=[];
         }
@@ -47,7 +49,7 @@ export default class OneGenre extends Component{
         } else {
             return (
                 <Fragment>
-                    <h2>Genre: </h2>
+                    <h2>Genre: {genreName} </h2>
 
                     <div className="list-group">
                         {movies.map((m) => (
