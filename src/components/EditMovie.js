572d4f892f1c74ab18c1a9b5b07478394ad26401
Raymond Jolly <raymond.jolly@gmail.com>
2021-09-18 01:21:35 -0400

    Updated the editMovie.js file to alert the user of a saved change or if an error has occurred.

--- src/components/EditMovie.js
+++ src/components/EditMovie.js
@@ -3,6 +3,7 @@ import Input from "./form-components/Input";
 import './EditMovie.css'
 import TextArea from "./form-components/TextArea";
 import Select from "./form-components/Select";
+import Alert from "./ui-components/Alert";
 
 
 export default class EditMovie extends Component{
@@ -28,7 +29,11 @@ export default class EditMovie extends Component{
             ],
             isLoaded:false,
             error: null,
-            errors:[]
+            errors:[],
+            alert:{
+                type: "d-none",
+                message:"",
+            }
         }
 
         this.handleChange=this.handleChange.bind(this);
@@ -59,7 +64,18 @@ export default class EditMovie extends Component{
 
         fetch('http://localhost:4000/v1/admin/editmovie', requestOptions)
             .then(response => response.json())
-            .then(data => console.log(data));
+            .then(data => {
+                if(data.error){
+                    this.setState({
+                        alert: {type: "alert-danger", message: data.error.message},
+                    })
+                } else{
+                    this.setState({
+                        alert: {type: "alert-success", message: "Changes saved"},
+                    })
+
+                }
+            });
     }
 
     handleChange =(evt) => {
@@ -128,6 +144,10 @@ export default class EditMovie extends Component{
             return (
                 <Fragment>
                     <h2>Add/Edit Movie</h2>
+                    <Alert
+                        alertType={this.state.alert.type}
+                        alertMessage={this.state.alert.message}
+                    />
                     <hr/>
                     <form onSubmit={this.handleSubmit}>
                         <input type="hidden" name="id" id="id" value={movie.id} onChange={this.handleChange}/>
@@ -180,10 +200,6 @@ export default class EditMovie extends Component{
                         <hr/>
                         <button className="btn btn-primary">Save</button>
                     </form>
-
-                    <div className="mt-3">
-                        <pre>{JSON.stringify(this.state, null, 3)}</pre>
-                    </div>
                 </Fragment>
             )
         }
