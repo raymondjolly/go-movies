a0bdbdac5173c064fd084adf0d7d242c2a74e8c2
Raymond Jolly <raymond.jolly@gmail.com>
2021-09-19 11:42:05 -0400

    Started the work to add delete Movie functionality

--- src/components/EditMovie.js
+++ src/components/EditMovie.js
@@ -1,145 +1,150 @@
-import React, {Component, Fragment} from "react";
+import React, { Component, Fragment } from "react";
+import "./EditMovie.css";
 import Input from "./form-components/Input";
-import './EditMovie.css'
 import TextArea from "./form-components/TextArea";
 import Select from "./form-components/Select";
 import Alert from "./ui-components/Alert";
+import {Link} from "react-router-dom";
 
-
-export default class EditMovie extends Component{
-
+export default class EditMovie extends Component {
     constructor(props) {
         super(props);
-        this.state={
-            movie:{
-                id:0,
-                title:"",
+        this.state = {
+            movie: {
+                id: 0,
+                title: "",
                 release_date: "",
-                runtime:"",
-                mpaa_rating:"",
-                rating:"",
-                description:""
+                runtime: "",
+                mpaa_rating: "",
+                rating: "",
+                description: "",
             },
-            mpaaOptions:[
-                {id:'G', value:'G'},
-                {id:'PG', value:'PG'},
-                {id:'PG13', value: 'PG13'},
-                {id:'R', value: 'R'},
-                {id:'NC17', value: 'NC17'}
+            mpaaOptions: [
+                { id: "G", value: "G" },
+                { id: "PG", value: "PG" },
+                { id: "PG13", value: "PG13" },
+                { id: "R", value: "R" },
+                { id: "NC17", value: "NC17" },
             ],
-            isLoaded:false,
+            isLoaded: false,
             error: null,
-            errors:[],
-            alert:{
+            errors: [],
+            alert: {
                 type: "d-none",
-                message:"",
-            }
-        }
+                message: "",
+            },
+        };
 
-        this.handleChange=this.handleChange.bind(this);
-        this.handleSubmit=this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleSubmit = (evt) =>{
+    handleSubmit = (evt) => {
         evt.preventDefault();
 
+        // client side validation
         let errors = [];
-        if(this.state.movie.title ===""){
+        if (this.state.movie.title === "") {
             errors.push("title");
         }
 
-        this.setState({errors:errors});
-        if(errors.length>0){
+        this.setState({ errors: errors });
+
+        if (errors.length > 0) {
             return false;
         }
 
         const data = new FormData(evt.target);
-        const payload = Object.fromEntries(data.entries())
-        console.log(payload)
+        const payload = Object.fromEntries(data.entries());
+        console.log(payload);
 
-        const requestOptions={
-            method:"POST",
-            body:JSON.stringify(payload)
-        }
+        const requestOptions = {
+            method: "POST",
+            body: JSON.stringify(payload),
+        };
 
-        fetch('http://localhost:4000/v1/admin/editmovie', requestOptions)
-            .then(response => response.json())
-            .then(data => {
-                if(data.error){
+        fetch("http://localhost:4000/v1/admin/editmovie", requestOptions)
+            .then((response) => response.json())
+            .then((data) => {
+                if (data.error) {
                     this.setState({
-                        alert: {type: "alert-danger", message: data.error.message},
-                    })
-                } else{
+                        alert: { type: "alert-danger", message: data.error.message },
+                    });
+                } else {
                     this.setState({
-                        alert: {type: "alert-success", message: "Changes saved"},
-                    })
-
+                        alert: { type: "alert-success", message: "Changes saved!" },
+                    });
                 }
             });
-    }
+    };
 
-    handleChange =(evt) => {
+    handleChange = (evt) => {
         let value = evt.target.value;
         let name = evt.target.name;
-        this.setState((prevState)=>({
-            movie:{
+        this.setState((prevState) => ({
+            movie: {
                 ...prevState.movie,
-                [name]:value,
-            }
-        }))
-    }
+                [name]: value,
+            },
+        }));
+    };
 
-    hasError(key){
-        return this.state.errors.indexOf(key)!==-1;
+    hasError(key) {
+        return this.state.errors.indexOf(key) !== -1;
     }
 
     componentDidMount() {
         const id = this.props.match.params.id;
-        if(id>0){
+        if (id > 0) {
             fetch("http://localhost:4000/v1/movie/" + id)
                 .then((response) => {
                     if (response.status !== "200") {
                         let err = Error;
-                        err.Message = "Invalid response code" + err;
-                        this.setState({error: err});
+                        err.Message = "Invalid response code: " + response.status;
+                        this.setState({ error: err });
                     }
                     return response.json();
                 })
-                .then((json)=>{
+                .then((json) => {
                     const releaseDate = new Date(json.movie.release_date);
+
                     this.setState(
                         {
-                            movie:{
-                                id:id,
+                            movie: {
+                                id: id,
                                 title: json.movie.title,
-                                releaseDate: releaseDate.toISOString().split("T")[0],
+                                release_date: releaseDate.toISOString().split("T")[0],
                                 runtime: json.movie.runtime,
                                 mpaa_rating: json.movie.mpaa_rating,
                                 rating: json.movie.rating,
-                                description: json.movie.description
+                                description: json.movie.description,
                             },
                             isLoaded: true,
                         },
-                        (error)=>{
+                        (error) => {
                             this.setState({
-                                isLoaded:true,
-                                error: error,
-                            })
+                                isLoaded: true,
+                                error,
+                            });
                         }
-                    )
-                })
-        } else{
-            this.setState({isLoaded:true});
+                    );
+                });
+        } else {
+            this.setState({ isLoaded: true });
         }
     }
 
-    render() {
-        let {movie, isLoaded, error} = this.state;
+    confirmDelete = (e)=>{
+        console.log("would delete movie id", this.state.movie.id)
+    }
 
-        if(error){
-            return <div>Error: {error.message}</div>
-        } else if(!isLoaded){
-            return <p>Loading...</p>
+    render() {
+        let { movie, isLoaded, error } = this.state;
+
+        if (error) {
+            return <div>Error: {error.message}</div>;
+        } else if (!isLoaded) {
+            return <p>Loading...</p>;
         } else {
             return (
                 <Fragment>
@@ -148,60 +153,78 @@ export default class EditMovie extends Component{
                         alertType={this.state.alert.type}
                         alertMessage={this.state.alert.message}
                     />
-                    <hr/>
+                    <hr />
                     <form onSubmit={this.handleSubmit}>
-                        <input type="hidden" name="id" id="id" value={movie.id} onChange={this.handleChange}/>
+                        <input
+                            type="hidden"
+                            name="id"
+                            id="id"
+                            value={movie.id}
+                            onChange={this.handleChange}
+                        />
+
                         <Input
-                            title={"Title"} className={this.hasError("title") ? "is-invalid" : ""}
-                            type={'text'}
-                            name={'title'}
+                            title={"Title"}
+                            className={this.hasError("title") ? "is-invalid" : ""}
+                            type={"text"}
+                            name={"title"}
                             value={movie.title}
                             handleChange={this.handleChange}
                             errorDiv={this.hasError("title") ? "text-danger" : "d-none"}
                             errorMsg={"Please enter a title"}
                         />
+
                         <Input
                             title={"Release Date"}
-                            type={'text'}
-                            name={'release_date'}
+                            type={"date"}
+                            name={"release_date"}
                             value={movie.release_date}
                             handleChange={this.handleChange}
                         />
+
                         <Input
                             title={"Runtime"}
-                            type={'text'}
-                            name={'runtime'}
+                            type={"text"}
+                            name={"runtime"}
                             value={movie.runtime}
                             handleChange={this.handleChange}
                         />
+
                         <Select
-                            title={'MPAA Rating'}
-                            name={'mpaa_rating'}
+                            title={"MPAA Rating"}
+                            name={"mpaa_rating"}
                             options={this.state.mpaaOptions}
                             value={movie.mpaa_rating}
                             handleChange={this.handleChange}
-                            placeholder={'Choose...'}
+                            placeholder={"Choose..."}
                         />
+
                         <Input
                             title={"Rating"}
-                            type={'text'}
-                            name={'rating'}
+                            type={"text"}
+                            name={"rating"}
                             value={movie.rating}
                             handleChange={this.handleChange}
                         />
+
                         <TextArea
                             title={"Description"}
-                            type={'text'}
-                            name={'description'}
+                            name={"description"}
                             value={movie.description}
-                            handleChange={this.handleChange}
                             rows={"3"}
+                            handleChange={this.handleChange}
                         />
-                        <hr/>
+
+                        <hr />
+
                         <button className="btn btn-primary">Save</button>
+                        <Link to="/admin" className="btn btn-warning ms-1">Cancel</Link>
+                        {movie.id >0 && (
+                            <a href="#!" onClick={()=>this.confirmDelete()} className='btn btn-danger ms-1'>Delete</a>
+                            )}
                     </form>
                 </Fragment>
-            )
+            );
         }
     }
 }
