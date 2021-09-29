eb9f7cad30c7421aed5c081a3288c1e333835f9d
Raymond Jolly <raymond.jolly@gmail.com>
2021-09-28 23:38:59 -0400

    added a GraphQL component that displays data from the provided API

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Genres from "./components/Genres";
 import OneGenre from "./components/OneGenre";
 import EditMovie from "./components/EditMovie";
 import Login from "./components/Login";
+import GraphQL from "./components/GraphQL";
 
 export default class App extends Component {
     constructor(props) {
@@ -82,6 +83,9 @@ export default class App extends Component {
                                             </li>
                                         </Fragment>
                                     )}
+                                    <li className="list-group-item">
+                                        <Link to="/graphql">GraphQL</Link>
+                                    </li>
                                 </ul>
                                 <pre>{JSON.stringify(this.state, null, 3)}</pre>
                             </nav>
@@ -107,6 +111,10 @@ export default class App extends Component {
 
                                 <Route exact path="/genres">
                                     <Genres />
+                                </Route>
+
+                                <Route exact path="graphql">
+                                    <GraphQL/>
                                 </Route>
 
                                 <Route
